c89855cee1241c21d965f5e00fdf4674e16a3a73
2443104892@qq.com <2443104892@qq.com>
2021-11-17 11:11:52 +0800

    去掉没用的引用

--- mybatisPlus/src/main/java/com/test/MyApplication.java
+++ mybatisPlus/src/main/java/com/test/MyApplication.java
@@ -1,7 +1,6 @@
 package com.test;
 
-import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
-import org.mybatis.spring.annotation.MapperScan;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
