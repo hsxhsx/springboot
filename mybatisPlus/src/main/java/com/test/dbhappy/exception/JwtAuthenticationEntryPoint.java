0363421d6328a185ea70bf534af8f4031cc4619c
2443104892@qq.com <2443104892@qq.com>
2021-11-18 17:53:46 +0800

    修改filter，增加异常

--- mybatisPlus/src/main/java/com/test/dbhappy/exception/JwtAuthenticationEntryPoint.java
+++ mybatisPlus/src/main/java/com/test/dbhappy/exception/JwtAuthenticationEntryPoint.java
@@ -4,7 +4,6 @@ import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 import org.springframework.stereotype.Component;
 
-import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,10 +14,8 @@ public class JwtAuthenticationEntryPoint implements AuthenticationEntryPoint {
     public void commence(HttpServletRequest request,
                          HttpServletResponse response,
                          AuthenticationException authException)
-            throws IOException, ServletException {
-
-        System.out.println("JwtAuthenticationEntryPoint:"+authException.getMessage());
-        response.sendError(HttpServletResponse.SC_UNAUTHORIZED,"没有凭证");
+            throws IOException{
+        response.sendError(HttpServletResponse.SC_UNAUTHORIZED);
     }
 }
 
